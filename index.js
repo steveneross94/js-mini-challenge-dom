2d884e74d50b93923d8bf731f71dde785edd24a8
steveneross94 <ross.steven@me.com>
2020-06-23 17:55:25 -0400

    first and second deliv

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 /***** Deliverable 1 *****/
-const header = document.querySelector("#header")
-console.log("Here's your header:", header)
+document.addEventListener("DOMContentLoaded", function(){
+    const header = document.querySelector("h1#header")
+    console.log("Here's your header:", header)
+
+})
+
 
 
 /***** Deliverable 2 *****/
-
+document.addEventListener("DOMContentLoaded", function(){
+    header.style.color = "red"
+})
 
 /***** Deliverable 3 *****/
-// console.log("PLAYERS array looks like this:", PLAYERS)
+console.log("PLAYERS array looks like this:", PLAYERS)
 
+for (const key in PLAYERS){
+    let playerObj = PLAYERS[key]
+    let playerDiv = document.createElement('div')
+
+    playerDiv.className = "player"
+    
+}
 
 /***** Deliverable 4 *****/
