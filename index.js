7260bb15ef3c50c84e438c935d9e28532eca219b
steveneross94 <ross.steven@me.com>
2020-06-23 21:43:25 -0400

    done

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 /***** Deliverable 1 *****/
 document.addEventListener("DOMContentLoaded", function(){
-    const header = document.querySelector("h1#header")
+    const header = document.querySelector("h1#header");
+    const playersDiv = document.querySelector("div.player-container")
     console.log("Here's your header:", header)
 
-})
+
+
 
 
 
@@ -15,12 +17,26 @@ document.addEventListener("DOMContentLoaded", function(){
 /***** Deliverable 3 *****/
 console.log("PLAYERS array looks like this:", PLAYERS)
 
+
 for (const key in PLAYERS){
     let playerObj = PLAYERS[key]
     let playerDiv = document.createElement('div')
 
     playerDiv.className = "player"
+    playerDiv.dataset.number = `${playerObj.number}`
+    playerDiv.innerHTML = `
+    <h3>
+        ${playerObj.name} (<em>${playerObj.nickname}</em>)
+    </h3>
+    <img src="${playerObj.photo} alt="${playerObj.name}">
+    `
+
+    playersDiv.append(playerDiv)
     
 }
 
 /***** Deliverable 4 *****/
+const playerSeven = document.querySelector("div[data-number='7']")
+console.log(playerSeven)
+playerSeven.remove()
+})
